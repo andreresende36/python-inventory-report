6a223744715d69df3f27cf3340344a4803934a5e
andreresende36 <andreresende36@gmail.com>
2023-08-26 15:51:31 -0300

    test 2: done

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,31 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    id = "1"
+    product_name = "Teclado mecânico"
+    company_name = "Logitech"
+    manufacturing_date = "10/01/2023"
+    expiration_date = "10/01/2033"
+    serial_number = "147852369"
+    storage_instructions = "Não-frágil"
+    product = Product(
+        id,
+        product_name,
+        company_name,
+        manufacturing_date,
+        expiration_date,
+        serial_number,
+        storage_instructions,
+    )
+    response = product.__str__()
+    expected_response = (
+        f"The product {id} - {product_name} "
+        f"with serial number {serial_number} "
+        f"manufactured on {manufacturing_date} "
+        f"by the company {company_name} "
+        f"valid until {expiration_date} "
+        "must be stored according to the following instructions: "
+        f"{storage_instructions}."
+    )
+    assert response == expected_response
