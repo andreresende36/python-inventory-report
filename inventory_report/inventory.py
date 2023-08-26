ebded79cb73b8aade794e712ffdc5a8a8750f3c8
andreresende36 <andreresende36@gmail.com>
2023-08-26 19:11:51 -0300

    feature 5: Inventory done

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,21 @@
+from typing import Optional
+
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[list[Product]] = None) -> None:
+        if not data:
+            self._data = []
+        else:
+            self._data = data
+
+    def add_data(self, data: list[Product]) -> None:
+        if not self._data:
+            self._data = data
+        else:
+            self._data += data
+
+    @property
+    def data(self) -> Optional[list[Product]]:
+        return self._data
