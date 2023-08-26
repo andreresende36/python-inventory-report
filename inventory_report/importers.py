5f828ef41c76d451714f6790972bc2fdc91c2f00
andreresende36 <andreresende36@gmail.com>
2023-08-26 16:32:14 -0300

    feature 4 and 8: JSON and CSV importers implemented

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,3 +1,5 @@
+import csv
+import json
 from abc import ABC, abstractmethod
 from typing import Dict, Type
 
@@ -13,12 +15,40 @@ class Importer(ABC):
         pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> list[Product]:
+        with open(self.path, "r") as file:
+            json_data: list[dict[str, str]] = json.load(file)
+            return [
+                Product(
+                    item["id"],
+                    item["product_name"],
+                    item["company_name"],
+                    item["manufacturing_date"],
+                    item["expiration_date"],
+                    item["serial_number"],
+                    item["storage_instructions"],
+                )
+                for item in json_data
+            ]
 
 
-class CsvImporter:
-    pass
+class CsvImporter(Importer):
+    def import_data(self) -> list[Product]:
+        with open(self.path, "r") as file:
+            csv_data = csv.DictReader(file)
+            return [
+                Product(
+                    item["id"],
+                    item["product_name"],
+                    item["company_name"],
+                    item["manufacturing_date"],
+                    item["expiration_date"],
+                    item["serial_number"],
+                    item["storage_instructions"],
+                )
+                for item in csv_data
+            ]
 
 
 # Não altere a variável abaixo
